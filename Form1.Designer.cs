02d55e6d8bfcc74b56e6158bcfd4ce8efbb3dc46
joseangelhernandez <ja.hernandezds@gmail.com>
2023-06-27 15:30:23 -0400

    PRIMERA INTERFAZ REALIZADA

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbEmailReceptor = new System.Windows.Forms.TextBox();
+            this.tbAsunto = new System.Windows.Forms.TextBox();
+            this.tbCuerpoMsg = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbEmailReceptor
+            // 
+            this.tbEmailReceptor.Font = new System.Drawing.Font("Montserrat", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbEmailReceptor.Location = new System.Drawing.Point(212, 91);
+            this.tbEmailReceptor.Name = "tbEmailReceptor";
+            this.tbEmailReceptor.PlaceholderText = "Escribir Receptor";
+            this.tbEmailReceptor.Size = new System.Drawing.Size(532, 29);
+            this.tbEmailReceptor.TabIndex = 0;
+            // 
+            // tbAsunto
+            // 
+            this.tbAsunto.Font = new System.Drawing.Font("Montserrat", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbAsunto.Location = new System.Drawing.Point(212, 173);
+            this.tbAsunto.Name = "tbAsunto";
+            this.tbAsunto.PlaceholderText = "Escribir Asunto";
+            this.tbAsunto.Size = new System.Drawing.Size(532, 29);
+            this.tbAsunto.TabIndex = 1;
+            // 
+            // tbCuerpoMsg
+            // 
+            this.tbCuerpoMsg.Font = new System.Drawing.Font("Montserrat Medium", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tbCuerpoMsg.Location = new System.Drawing.Point(212, 264);
+            this.tbCuerpoMsg.Multiline = true;
+            this.tbCuerpoMsg.Name = "tbCuerpoMsg";
+            this.tbCuerpoMsg.PlaceholderText = "Escribir Mensaje";
+            this.tbCuerpoMsg.Size = new System.Drawing.Size(532, 353);
+            this.tbCuerpoMsg.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(103, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 27);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Para:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(75, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 27);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Asunto:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Montserrat SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(75, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 27);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mensaje:";
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Font = new System.Drawing.Font("Montserrat SemiBold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnEnviar.Location = new System.Drawing.Point(212, 641);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(342, 71);
+            this.btnEnviar.TabIndex = 6;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Montserrat SemiBold", 14.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(560, 641);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 71);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Cancelar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 738);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbCuerpoMsg);
+            this.Controls.Add(this.tbAsunto);
+            this.Controls.Add(this.tbEmailReceptor);
+            this.Name = "Form1";
+            this.Text = "EMAIL SENDER - JJ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox tbEmailReceptor;
+        private TextBox tbAsunto;
+        private TextBox tbCuerpoMsg;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button btnEnviar;
+        private Button button1;
     }
 }
